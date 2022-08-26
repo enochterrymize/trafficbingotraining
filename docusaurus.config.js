80b2c959a487b8df6f4f0fbc2eb6c85780df901b
Terry <enochterrymize@outlook.com>
2022-08-26 14:15:10 +0200

    config updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Welcome To Your 'Traffic Bingo' Training',
+  title: 'Welcome To Your Traffic Bingo Training',
   tagline: 'Reaveled Traffic Blueprint ',
   url: 'https://trafficbingo.com',
   baseUrl: '/',
